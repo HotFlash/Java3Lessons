d60c248ab9980dfdc702e9dffb644728e0ae0879
Roman Nabokov <roman@newage.kz>
2022-09-11 17:10:09 +0600

    HomeWork3

--- Server/src/main/java/ServerApp.java
+++ Server/src/main/java/ServerApp.java
@@ -2,7 +2,7 @@ import server.MyServer;
 
 public class ServerApp {
 
-    private static final int PORT = 8190;
+    private static final int PORT = 8188;
 
     public static void main(String[] args) {
         new MyServer().start(PORT);
